47e412708bc99fcb34db0e21305db2d878041ac9
Hector Rodriguez <hectiquin2003@hotmail.com>
2022-08-15 13:36:32 -0400

    fix issue with AZs

--- eks/data.tf
+++ eks/data.tf
@@ -1,5 +1,14 @@
 # get all available AZs in our region
 data "aws_availability_zones" "available_azs" {
-	state = "available"
+  state = "available"
+  filter {
+    name   = "zone-name"
+    values = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f"]
+  }
 }
 data "aws_caller_identity" "current" {} # used for accesing Account ID and ARN
+
+#  filter {
+#    name   = "name"
+#    values = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f"]
+#  }
