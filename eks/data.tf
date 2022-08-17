150dabea17cac238eb771ebe1fa9b8a638e1ba49
Hector Rodriguez <hectiquin2003@hotmail.com>
2022-08-17 10:54:43 -0400

    modified

--- eks/data.tf
+++ eks/data.tf
@@ -8,7 +8,13 @@ data "aws_availability_zones" "available_azs" {
 }
 data "aws_caller_identity" "current" {} # used for accesing Account ID and ARN
 
-#  filter {
-#    name   = "name"
-#    values = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f"]
-#  }
+
+#To fix issue,Error: the server could not find the requested resource (post configmaps)
+
+# get EKS cluster info to configure Kubernetes and Helm providers
+  data "aws_eks_cluster" "cluster" {
+  name = module.eks_cluster.cluster_id
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks_cluster.cluster_id
+}
--- eks/providers.tf
+++ eks/providers.tf
@@ -9,3 +9,12 @@ terraform {
 provider "aws" {
     region = var.region
 }
+
+#To fix issue,Error: the server could not find the requested resource (post configmaps)
+
+# get EKS authentication for being able to manage k8s objects from terraform
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
